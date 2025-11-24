0dc3be749c49a690b9f3729f6487ef597dd2df52
iokitpusher <tt611@live.mdx.ac.uk>
2025-11-24 20:00:02 +0100

    readded cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./db.js";
 
 dotenv.config();
@@ -8,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+
+app.use(cors()); //for now lets test frontedn fully functional etc
 
 app.get("/", (_req, res) => {
   res.send("ok");
